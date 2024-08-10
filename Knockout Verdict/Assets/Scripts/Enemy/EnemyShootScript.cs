3c6dbe9d6fba1f9972ae828cf91f8cd78672e019
Pinaki Jha <pinakijah@gmail.com>
2024-08-10 19:08:41 +0530

    minor bug fixes

--- Knockout Verdict/Assets/Scripts/Enemy/EnemyShootScript.cs
+++ Knockout Verdict/Assets/Scripts/Enemy/EnemyShootScript.cs
@@ -1,6 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Tilemaps;
+//using System.Collections.Generic;
+//using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyShootScript : MonoBehaviour
